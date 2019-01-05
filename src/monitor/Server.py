9ba438b4fbeefbd83129b6edb4bb2c5a8296f1f6
paveldriver <paveldriver0@gmail.com>
2019-01-05 21:45:13 +0300

    Заготовка под сервер

---
+++ src/monitor/Server.py
@@ -0,0 +1,25 @@
+import time
+from threading import Thread
+
+
+class Server(Thread):
+    """
+    A threading example
+    """
+
+    def __init__(self, name, monitor):
+        """Инициализация потока"""
+        Thread.__init__(self)
+        self.name = name
+        self._monitor = monitor
+
+    def run(self):
+        """Запуск потока"""
+        msg = "%s is running" % self.name
+        print(msg)
+        self.check()
+
+    def check(self):
+        while self._monitor._state == 1:
+            print("run")
+            time.sleep(self._monitor.get_timeout())
---
+++ src/test/TestMonitor.py
@@ -0,0 +1,16 @@
+import unittest
+from src.monitor.Monitor import Monitor
+import time
+
+
+class TestMonitor(unittest.TestCase):
+
+    def setUp(self):
+        self.monitor = Monitor()
+        self.monitor.start()
+
+    def test_stop_monitoring(self):
+        print(self.monitor.get_state())
+        time.sleep(10)
+        execute_code = self.monitor.stop()
+        self.assertEqual(0, execute_code)
